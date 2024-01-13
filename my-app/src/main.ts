8a8120810af6764a4c78ce84b7384ebf12657d11
Caleb <100630232+Calebroe@users.noreply.github.com>
2024-01-13 18:08:50 -0500

    removing node modules

--- my-app/src/main.ts
+++ my-app/src/main.ts
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router';
 
-createApp(App).mount('#app')
+// Firebase setup
+import { initializeApp } from 'firebase/app';
+import { Firestore, getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiPbIRyQ4A2GjWpS8t6yM_xyTbhInRnwo",
+  authDomain: "not-so-jolly-emporium.firebaseapp.com",
+  projectId: "not-so-jolly-emporium",
+  storageBucket: "not-so-jolly-emporium.appspot.com",
+  messagingSenderId: "289922164067",
+  appId: "1:289922164067:web:703f5c13ade5c50735df56"
+};
+
+const app = createApp(App);
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+app.use(router);
+app.provide('db', db);
+app.mount('#app');
+
+
+
---
+++ my-app/src/router.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory} from 'vue-router';
+import App from './App.vue'
+
+const routes = [
+  {
+    path: "/",
+    component: Base,
+    children: [
+      { path: "", redirect: { name: "login" } }, // Redirect to the 'dashboard' named route
+      { path: "login", name: "login", component: Login}, // Corrected path (removed leading '/')
+    ],
+  },
+  {
+    path: "/app",
+    component: App,
+    children: [ 
+      { path: "", redirect: { name: "home" } }, // Redirect to the 'dashboard' named route
+      { path: '/home', component: Home, name: 'home' },
+      { path: '/category/bestsellers', component: Bestsellers, name: 'bestsellers' },
+      { path: '/category/new', component: New, name: 'new' },
+      { path: '/category/ships', component: Ships, name: 'ships' },
+      { path: '/category/cannons', component: Cannons, name: 'cannons' },
+      { path: '/category/firearms', component: Firearms, name: 'firearms' },
+      { path: '/category/swords', component: Swords, name: 'swords' },
+      { path: '/category/clothes', component: ClothesVue, name: 'clothes' },
+      { path: '/category/sale', component: Sale, name: 'sale' },
+      // ... any other child routes
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
